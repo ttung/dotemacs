33fcfadb5eda6370a0e5ee54ff141cbdf6e1bba6
Tony Tung <tonytung@merly.org>
2005-10-11 04:06:48 +0000

    compilation script.

---
+++ elisp/mmm-mode/subdir.mk
@@ -0,0 +1,23 @@
+LISP_FILES = 	mmm-auto.el \
+		mmm-class.el \
+		mmm-cmds.el \
+		mmm-compat.el \
+		mmm-cweb.el \
+		mmm-mason.el \
+		mmm-mode.el \
+		mmm-noweb.el \
+		mmm-region.el \
+		mmm-rpm.el \
+		mmm-sample.el \
+		mmm-univ.el \
+		mmm-utils.el \
+		mmm-vars.el \
+
+
+ELC_FILES = $(LISP_FILES:.el=.elc)
+
+all	:	$(ELC_FILES)
+
+
+%.elc	:	%.el subdir.mk
+	emacs -q -batch --eval '(setq load-path (cons nil load-path))' -f batch-byte-compile "$<"
